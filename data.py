d64af2453b31b00c6858a85d9d0590ce0b9674bb
edw_flores <hpg9sppwrj@privaterelay.appleid.com>
2026-02-07 14:56:29 -0600

    first commit

---
+++ data.py
@@ -0,0 +1,70 @@
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+st.set_page_config(page_title='Edwin Flores - Life Vlog', layout='wide')
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_coding = load_lottieurl("https://lottie.host/95aed5c3-e2e5-4b51-84d7-e6b57f18e58b/Uz8Ra9ImHx.json")
+img_contact_form = 'Ideal Rocket Equation_1.png'
+img_contact_form_2 = 'Ideal Rocket Equation_2.png'
+
+st.subheader("Hi, I'm Edwin Flores :v:")
+st.title('An Aspiring Aerospace Engineer, Musician, and Life Vlogger')
+st.write("""
+         I'm passionate about creating content that combines my interests in technology, 
+         music, and storytelling. Join me on my journey as I explore the world of coding, 
+         share my musical experiences, and document my life adventures!
+         """)
+st.write("[My GitHub >](https://github.com/hpg9sppwrj-wq)")
+
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("About Me")
+        st.write("""
+                 I'm Edwin Flores, an aspiring aerospace engineer with a love for 
+                 music and vlogging. I enjoy sharing my knowledge and experiences 
+                 through engaging content. Whether it's coding projects, music covers, 
+                 or life vlogs, I aim to inspire and connect with my audience.
+                 """)
+        st.write("[YouTube Channel >](https://youtube.com/@edw_flores10)")
+    with right_column:
+        st_lottie(lottie_coding, height=200, key="coding")
+
+with st.container():
+    st.write("---")
+    st.header("My Projects")
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(img_contact_form)
+    
+    with text_column:
+        st.subheader('Aerospace for Python | Ideal Rocket Equation - Part 1')
+        st.write("""
+                In this video, we program a calculator in Python to find the change in velocity
+                of a rocket using the Ideal Rocket Equation. This is part one of a series on aerospace
+                engineering concepts implemented in Python.
+                 """)
+        st.markdown('[Watch Video >](https://www.youtube.com/watch?v=f4-miRHYeEc)')
+
+with st.container():
+    image_column, text_column = st.columns((1,2))
+    with image_column:
+        st.image(img_contact_form_2)
+
+    with text_column:
+        st.subheader('Aerospace for Python | Ideal Rocket Equation - Part 2')
+        st.write("""
+                In this video, we continue our exploration of the Ideal Rocket Equation by implementing a
+                calculator in Python to determine the mass ratio of a rocket based on your desired change in
+                velocity. This is part two of our aerospace engineering series using Python.
+                 """)
+        st.markdown('[Watch Video >](https://www.youtube.com/watch?v=W7SyLYSv5NQ)')
